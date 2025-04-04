48c503f7a5294a3f5c4154d584a5736f687db666
bryantewyh <bryantewyh@gmail.com>
2025-04-04 22:12:54 +0800

    Fixed structure

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ mod sensor_module;
 fn main() {
     sensor_module::main::sensor_start();
     actuator_module::main::actuator_start();
-    println!("helo")
+    println!("helo");
 }
--- src/sensor_module/main.rs
+++ src/sensor_module/main.rs
@@ -1,3 +1,6 @@
-pub fn sensor_start(){
+use crate::sensor_module::sensor;
+
+pub async fn sensor_start(){
     print!("Sensor module started\n");
+    sensor::force_sensor();
 }
