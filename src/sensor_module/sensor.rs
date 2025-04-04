b77553105ff3a15bde75f0e8b0c72cd18667cfc3
bryantewyh <bryantewyh@gmail.com>
2025-04-04 20:42:52 +0800

    Dependencies Update 1
    Added dependencies
    Added structs
    Changed test.rs > sensor.rs

---
+++ src/sensor_module/sensor.rs
@@ -0,0 +1,29 @@
+
+struct WristData {
+    wrist_x: c_float,
+    wrist_y: c_float,
+    wrist_z: c_float,
+}
+
+struct JointData {
+    shoulder_x: c_float,
+    shoulder_y: c_float,
+    shoulder_z: c_float,
+}
+
+struct ElbowData {
+    elbow_x: c_float,
+    elbow_y: c_float,
+    elbow_z: c_float,
+}
+
+struct SensorArmData {
+    force_data: c_float,
+    wrist: WristData,
+    joints: JointData,
+    elbow: ElbowData,
+}
+
+pub fn sensor() {
+        
+}
--- src/sensor_module/test.rs
+++
@@ -1,4 +0,0 @@
-pub fn test() {
-    println!("Sensor module test function called.");
-    // Add your test logic here
-}
