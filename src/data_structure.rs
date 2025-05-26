f0743aed18006e9fa9beee5ba98961977dcca1c4
Phandastick <slugslingermario@gmail.com>
2025-05-26 22:25:38 +0800

    Update data_structure.rs
    Update data_structure.rs

--- src/data_structure.rs
+++ src/data_structure.rs
@@ -1,36 +1,47 @@
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct SensorArmData {
-    pub force_data: f32,
+    pub object_data: ObjectData,
+
     pub wrist: WristData,
     pub joints: ShoulderData,
     pub elbow: ElbowData,
+    pub arm_velocity: f32,
+    //higher speed, more strength
+    pub arm_strength: f32, // use speed to calculate force of arm
+
     pub timestamp: u128,
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct ObjectData {
+    pub object_velocity: f32,
+    pub object_mass: f32,
+    pub object_size: f32,
+    pub object_distance: f32, //height from robotic arm
+}
+
+//stored in sensor
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct WristData {
     pub wrist_x: f32,
     pub wrist_y: f32,
-    pub wrist_z: f32,
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct ShoulderData {
     pub shoulder_x: f32,
     pub shoulder_y: f32,
-    pub shoulder_z: f32,
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct ElbowData {
     pub elbow_x: f32,
     pub elbow_y: f32,
-    pub elbow_z: f32,
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-pub struct FeedbackData {     
-    pub status: String,        
-    pub adjustment_value: f64, 
+pub struct FeedbackData {
+    pub status: String,
+    pub adjustment_value: f64,
     pub timestamp: u128,
 }
